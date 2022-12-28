e9fc2398d52bd2a7e306859c1ff9d50c097da38b
Anar Bayramov <bayramovanar1703@gmail.com>
2022-12-29 02:45:39 +0400

    added css to pagination

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import MovieList from "./MovieList";
+import MovieInfo from "./MovieInfo";
+import Contact from "./Contact";
+import NotFoundPage from "./NotFoundPage";
+import Navbar from "./Navbar";
+
+export default function App() {
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="/" element=<Home /> />
+                <Route path="/movies" element=<MovieList /> />
+                <Route path="/movies?page=:pageNum" element=<MovieList /> />
+                <Route path="/movies/:movieId" element=<MovieInfo /> />
+                <Route path="/contact" element=<Contact /> />
+                <Route path="/*" element=<NotFoundPage /> />
+            </Routes>
+        </>
+    );
+}
---
+++ src/components/Contact.jsx
@@ -0,0 +1,48 @@
+import React, { useRef } from 'react';
+import useForm from "./UseForm";
+
+export default function Contact() {
+    const formElement = useRef(null);
+    const additionalData = {
+        sent: new Date().toISOString(),
+    };
+
+    const { handleSubmit, status, message } = useForm({
+        form: formElement.current,
+        additionalData,
+    });
+
+    if (status === "success") {
+        return (
+            <>
+                <div className="text-2xl">Thank you!</div>
+                <div className="text-md">{message}</div>
+            </>
+        );
+    }
+
+    if (status === "error") {
+        return (
+            <>
+                <div className="text-2xl">Something bad happened!</div>
+                <div className="text-md">{message}</div>
+            </>
+        );
+    }
+
+    return (
+        <div className="contact">
+            <form action="http://localhost:3002/messages" onSubmit={handleSubmit} method="POST" ref={formElement}>
+                <label hmtlfor="email">From: </label>
+                <input type="email" name="email" id="email" />
+
+                <label hmtlfor="subject">Subject: </label>
+                <input type="text" name="subject" id="subject" />
+
+                <label hmtlfor="message">Message: </label>
+                <textarea name="text" id="message" cols="30" rows="10" placeholder="Type Here..."></textarea>
+                <input type="submit" value="SUBMIT" />
+            </form>
+        </div >
+    );
+}
